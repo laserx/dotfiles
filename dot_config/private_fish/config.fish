607c7334757921197a529b284ecc277dbb9d78d6
laserx <laserxie@gmail.com>
2022-01-05 14:00:00 +0800

    init

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    # set gopath and gobin
+    set -x GOPATH (go env GOPATH)
+    set -x PATH $GOPATH/bin $PATH
+    # set yarn
+    set -x PATH (yarn global bin) $PATH
+    # set mix
+    set -x PATH $HOME/.mix $PATH
+    # Alias
+    alias hp="https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890 "
+
+    # starship
+    starship init fish | source
+end
---
+++ dot_config/private_fish/functions/fish_greeting.fish
@@ -0,0 +1,4 @@
+function fish_greeting
+    echo 现在 (set_color yellow; date +%T; set_color normal)
+    echo 快点干活!
+end
